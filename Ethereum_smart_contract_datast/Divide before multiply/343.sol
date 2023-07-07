c5b0d9d86c7c3052fa1950140b83eeca9e4e6b53
32Nitesh <nk543729@gmail.com>
2023-07-07 19:00:53 +0530

    07-07-2023

---
+++ Ethereum_smart_contract_datast/Divide before multiply/343.sol
@@ -0,0 +1,22 @@
+function getTokens() public payable canDistr onlyWhitelist {
+    if (value > totalRemaining) {
+        value = totalRemaining;
+    }
+
+    require(value <= totalRemaining);
+
+    address investor = msg.sender;
+    uint256 toGive = value;
+
+    distr(investor, toGive);
+
+    if (toGive > 0) {
+        blacklist[investor] = true;
+    }
+
+    if (totalDistributed >= totalSupply) {
+        distributionFinished = true;
+    }
+
+    value = value.div(100000).mul(99999);//Divide before multiply
+}
