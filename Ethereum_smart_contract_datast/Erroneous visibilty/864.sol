a2049a038c5dc24513554bb945fb59c7e185fe4d
32Nitesh <nk543729@gmail.com>
2023-07-29 19:11:52 +0530

    29-07-2023

---
+++ Ethereum_smart_contract_datast/Erroneous visibilty/864.sol
@@ -0,0 +1,32 @@
+contract WithdrawableToken {
+    function transfer(address _to, uint _value) returns (bool success);
+
+    function balanceOf(address _owner) constant returns (uint balance);
+}
+
+function burn(uint256 _value) returns (bool success) {
+    if (balances[msg.sender] < _value) revert(); // Check if the sender has enough
+    if (_value <= 0) revert();
+    balances[msg.sender] = SafeMath.safeSub(balances[msg.sender], _value); // Subtract from the sender
+    totalSupply = SafeMath.safeSub(totalSupply, _value); // Updates totalSupply
+    Burn(msg.sender, _value);
+    return true;
+}
+
+function freeze(uint256 _value) returns (bool success) {
+    if (balances[msg.sender] < _value) revert(); // Check if the sender has enough
+    if (_value <= 0) revert();
+    balances[msg.sender] = SafeMath.safeSub(balances[msg.sender], _value); // Subtract from the sender
+    freezes[msg.sender] = SafeMath.safeAdd(freezes[msg.sender], _value); // Updates totalSupply
+    Freeze(msg.sender, _value);
+    return true;
+}
+
+function unfreeze(uint256 _value) returns (bool success) {
+    if (freezes[msg.sender] < _value) revert(); // Check if the sender has enough
+    if (_value <= 0) revert();
+    freezes[msg.sender] = SafeMath.safeSub(freezes[msg.sender], _value); // Subtract from the sender
+    balances[msg.sender] = SafeMath.safeAdd(balances[msg.sender], _value);
+    Unfreeze(msg.sender, _value);
+    return true;
+}
