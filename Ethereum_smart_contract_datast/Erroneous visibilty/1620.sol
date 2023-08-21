422f758de63935d5994b7b8a87065a014c5ea152
32Nitesh <nk543729@gmail.com>
2023-08-21 19:02:23 +0530

    21-08-2023

--- Ethereum_smart_contract_datast/Erroneous visibilty/1620.sol
+++ Ethereum_smart_contract_datast/Erroneous visibilty/1620.sol
@@ -1,9 +1,9 @@
-function CmmToken() {
-    totalSupply = 1800000000 * (10 ** (uint256(decimals)));
-    balances[msg.sender] = totalSupply; // Give the creator all initial tokens
-}
+    function CmmToken() {
+        totalSupply = 1800000000 * (10 ** (uint256(decimals)));
+        balances[msg.sender] = totalSupply; // Give the creator all initial tokens
+    }
 
-function() {
-    //if ether is sent to this address, send it back.
-    revert();
-}
+    function() {
+        //if ether is sent to this address, send it back.
+        revert();
+    }
